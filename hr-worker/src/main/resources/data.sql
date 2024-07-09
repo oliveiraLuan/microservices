c384c9914dcdb880ece6277e3fad37e076a9582d
Luan de Oliveira <luan012012@hotmail.com>
2024-07-08 22:17:03 -0300

    feat: adicionado seed para o banco

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker(name, daily_income) VALUES ('Luan de Oliveira', 3500.0);
+INSERT INTO tb_worker(name, daily_income) VALUES ('Chuck Noland', 12000.0);
+INSERT INTO tb_worker(name, daily_income) VALUES ('Viktor Navorski', 3000.0);
