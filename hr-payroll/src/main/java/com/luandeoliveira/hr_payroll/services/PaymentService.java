a9cf697487c65b10a3b0767ca1195ee26a18d0cf
Luan de Oliveira <luan012012@hotmail.com>
2024-07-12 23:42:03 -0300

    refactor: removido restTemplate e utilizado WorkerFeignClient

--- hr-payroll/src/main/java/com/luandeoliveira/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/luandeoliveira/hr_payroll/services/PaymentService.java
@@ -1,33 +1,19 @@
 package com.luandeoliveira.hr_payroll.services;
 
+import com.luandeoliveira.hr_payroll.dto.WorkerDTO;
 import com.luandeoliveira.hr_payroll.entities.Payment;
 import com.luandeoliveira.hr_payroll.entities.Worker;
+import com.luandeoliveira.hr_payroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
-
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Value("${hr-worker-host}")
-    private String hrWorkerHost;
-
-    private static final String WORKERS_ENDPOINT = "/workers/{id}";
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long workerId, Integer days){
-
-        Map<String, String> pathVariables = new HashMap<>();
-        pathVariables.put("id", workerId.toString());
-
-        Worker worker = restTemplate.getForObject(hrWorkerHost + WORKERS_ENDPOINT, Worker.class, pathVariables);
-
+        WorkerDTO worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
