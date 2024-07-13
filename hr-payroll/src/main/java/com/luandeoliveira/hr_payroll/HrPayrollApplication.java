e2331bcd465ac6a90058961f1daf98b19da4956f
Luan de Oliveira <luan012012@hotmail.com>
2024-07-13 19:15:05 -0300

    style: removido import não utilizado

--- hr-payroll/src/main/java/com/luandeoliveira/hr_payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/luandeoliveira/hr_payroll/HrPayrollApplication.java
@@ -3,8 +3,8 @@ package com.luandeoliveira.hr_payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
