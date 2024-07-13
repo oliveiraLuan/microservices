b730123391974d9847a73e6bf53e9c001d714227
Luan de Oliveira <luan012012@hotmail.com>
2024-07-13 19:30:55 -0300

    feat: adicionado método alternativo para tolerância de falhas

--- hr-payroll/src/main/java/com/luandeoliveira/hr_payroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/luandeoliveira/hr_payroll/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package com.luandeoliveira.hr_payroll.controllers;
 
 import com.luandeoliveira.hr_payroll.entities.Payment;
 import com.luandeoliveira.hr_payroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,15 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentFallback")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentFallback(Long workerId, Integer days){
+        Payment mockedPayment = new Payment("Luan", 100.0, days);
+        return ResponseEntity.ok(mockedPayment);
     }
 }
