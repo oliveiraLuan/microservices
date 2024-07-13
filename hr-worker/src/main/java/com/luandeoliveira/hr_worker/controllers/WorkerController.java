f8503a80900664d2ea8f051be1134030a62f1ad7
Luan de Oliveira <luan012012@hotmail.com>
2024-07-13 12:47:53 -0300

    feat: adicionado log para imprimir qual instância está sendo utilizada

--- hr-worker/src/main/java/com/luandeoliveira/hr_worker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/luandeoliveira/hr_worker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package com.luandeoliveira.hr_worker.controllers;
 
 import com.luandeoliveira.hr_worker.dto.WorkerDTO;
 import com.luandeoliveira.hr_worker.services.WorkerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +21,11 @@ public class WorkerController {
     @Autowired
     private WorkerService workerService;
 
+    @Autowired
+    private Environment environment;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkerController.class);
+
     @GetMapping
     public ResponseEntity<List<WorkerDTO>> findAll(){
         List<WorkerDTO> workers = workerService.findAll();
@@ -26,6 +34,7 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<WorkerDTO> findById(@PathVariable Long id){
+        LOGGER.info("PORTA = " + environment.getProperty("local.server.port"));
         WorkerDTO dto = workerService.findById(id);
         return ResponseEntity.ok(dto);
     }
