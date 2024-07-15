df6610487f464bb6c6b4206f7cda7f0c9dfd7b6c
Luan de Oliveira <luan012012@hotmail.com>
2024-07-15 13:06:32 -0300

    feat: adicionado @RefreshScope para receber atualizações do config server via actuator

--- hr-worker/src/main/java/com/luandeoliveira/hr_worker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/luandeoliveira/hr_worker/controllers/WorkerController.java
@@ -5,6 +5,8 @@ import com.luandeoliveira.hr_worker.services.WorkerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value ="/workers")
 public class WorkerController {
+    @Value(value = "${test.config}")
+    private String testConfig;
 
     @Autowired
     private WorkerService workerService;
@@ -25,6 +30,12 @@ public class WorkerController {
     private Environment environment;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WorkerController.class);
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        LOGGER.info("CONFIGS = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<WorkerDTO>> findAll(){
