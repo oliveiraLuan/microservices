fb462b5712299558f95198636112568ae2bf9aa7
Luan de Oliveira <luan012012@hotmail.com>
2024-07-18 21:23:40 -0300

    feat: criado UserController

---
+++ hr-user/src/main/java/com/luandeoliveira/hr_user/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.luandeoliveira.hr_user.controllers;
+
+import com.luandeoliveira.hr_user.dto.UserDTO;
+import com.luandeoliveira.hr_user.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        UserDTO dto = userService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<UserDTO> findByEmail(@RequestParam String email){
+        UserDTO dto = userService.findByEmail(email);
+        return ResponseEntity.ok(dto);
+    }
+}
