4f44a6f9ea2088adebb2e924f970f75a704b7eda
Luan de Oliveira <luan012012@hotmail.com>
2024-07-23 13:30:02 -0300

    feat: criado AuthorizationServerConfig

---
+++ hr-oauth/src/main/java/com/luandeoliveira/hr_oauth/config/AuthorizationServerConfig.java
@@ -0,0 +1,58 @@
+package com.luandeoliveira.hr_oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private static final String PASSWORD = "PASSWORD";
+
+    private static final String READ = "READ";
+
+    private static final String WRITE = "WRITE";
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenStore jwtTokenStore;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("myclientid")
+                .secret(bCryptPasswordEncoder.encode("myclientsecret"))
+                .scopes(READ, WRITE)
+                .authorizedGrantTypes(PASSWORD)
+                .accessTokenValiditySeconds(86400);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(jwtTokenStore)
+                .accessTokenConverter(accessTokenConverter);
+    }
+}
