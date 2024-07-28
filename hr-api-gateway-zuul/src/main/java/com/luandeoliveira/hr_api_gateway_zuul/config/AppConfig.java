dad3e2e22eb2a04fdb411de1755de4f52764ce26
Luan de Oliveira <luan012012@hotmail.com>
2024-07-27 22:43:47 -0300

    refactor: externalizado jwt secret

--- hr-api-gateway-zuul/src/main/java/com/luandeoliveira/hr_api_gateway_zuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/luandeoliveira/hr_api_gateway_zuul/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.luandeoliveira.hr_api_gateway_zuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -7,10 +8,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+    @Value(value = "${jwt.secret}")
+    private String jwtSecret;
+
     @Bean
     public JwtAccessTokenConverter accessTokenConverter(){
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("SECRET_KEY");
+        jwtAccessTokenConverter.setSigningKey(jwtSecret);
         return jwtAccessTokenConverter;
     }
 
